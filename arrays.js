aa3fb20e4b0a0d58d472437fe999fb6118648327
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-26 02:54:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,13 +2,13 @@
 TODO: In arrays.js, define a variable called chocolateBars. Its value should be an array of the strings snickers, hundred grand, kitkat, and skittles.
 */
 
-var chocolateBars = new Array('snickers, hundred grand, kitkat, and skittles')
+var chocolateBars = new Array('snickers, hundred grand, kitkat, and skittles');
 
-var cities = ["New York", "San Francisco"];
+cities = ["New York", "San Francisco"];
 
-function addElementToBeginningOfArray(array, element)
+function addElementToBeginningOfArray(array,element)
 {
-array[...element];
+y[...element];
 }
 
 addElementToBeginningOfArray(cities,"Miami");
