2727a59854abf03c241fe3a10772c4abcf8228f2
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-26 02:47:09 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+/*
+TODO: In arrays.js, define a variable called chocolateBars. Its value should be an array of the strings snickers, hundred grand, kitkat, and skittles.
+*/
+
+var chocolateBars = new Array('snickers, hundred grand, kitkat, and skittles')
