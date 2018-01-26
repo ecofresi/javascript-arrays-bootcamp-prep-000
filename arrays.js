5b71fd599bdac4d519ca5cec14c6a3b506e9388b
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-26 02:55:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@
 TODO: In arrays.js, define a variable called chocolateBars. Its value should be an array of the strings snickers, hundred grand, kitkat, and skittles.
 */
 
-var chocolateBars = new Array('snickers, hundred grand, kitkat, and skittles');
+var chocolateBars = new Array('snickers, hundred grand, kitkat, skittles');
 
 cities = ["New York", "San Francisco"];
 
